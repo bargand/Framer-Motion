09df6e7bc6df9dd6c624a74f810109307c887691
Debargha Nandi <debarghanandi.india@gmail.com>
2025-02-03 00:13:53 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { motion } from "motion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <motion.div
+        className="box"
+        animate={{ 
+          x:[0,500,500,0,0],
+          y:[0,0,200,200,0]
+        }}
+        transition={{ duration: 4, repeat: Infinity, ease: "anticipate" }}
+      ></motion.div>
+    </div>
+  );
+};
 
-export default App
+export default App;
